344ae7383ceea24e2766a090a1d2ad15996690ad
David A. Parry <david.parry@ed.ac.uk>
2021-12-13 15:03:34 +0000

    initial commit

---
+++ workflow/scripts/ref_from_xlsx.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_excel(snakemake.input[0])
+df['reference_name'] = df.GUIDE_ID.apply(
+    lambda x: x.replace(':', '_').replace('-', '_', 1).replace(
+        '-', 'minus', 1).replace('+', 'plus', 1)) + "_" + df['TARGET EXON']
+with open(snakemake.output[0], 'wt') as fh:
+    df.apply(lambda x: fh.write(">{}\n{}\n".format(x.reference_name,
+                                                   x.SEQUENCE)),
+             axis=1)
