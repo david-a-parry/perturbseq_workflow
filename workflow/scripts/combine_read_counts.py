9ccb62655394ce32aa025e14f6f233345699e6a0
David A. Parry <david.parry@ed.ac.uk>
2021-12-15 17:02:59 +0000

    Calculate read fractions per sample

--- workflow/scripts/combine_read_counts.py
+++ workflow/scripts/combine_read_counts.py
@@ -12,4 +12,8 @@ df.to_csv("results/read_counts_per_alignment.csv.gz", index=False)
 count_df = df.groupby(
     ['sample_name', 'Target', 'Gene'])['Coverage'].sum().to_frame()
 count_df.reset_index(inplace=True)
+samp2mapped = count_df.groupby(['sample_name'])['Coverage'].sum().to_dict()
+count_df['fraction_reads'] = count_df.apply(
+    lambda x: x.Coverage/samp2mapped[x.sample_name],
+    axis=1)
 count_df.to_csv("results/read_counts_per_sample.csv.gz", index=False)
