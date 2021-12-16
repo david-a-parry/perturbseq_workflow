950174add0e96776e657a8d14581e146a0b58beb
David A. Parry <david.parry@ed.ac.uk>
2021-12-16 23:34:00 +0000

    add regression analyses

--- workflow/scripts/prepare_jacks_maps.py
+++ workflow/scripts/prepare_jacks_maps.py
@@ -9,11 +9,11 @@ with open(snakemake.output[0], 'wt') as fh:
     for gt in sample_df.genotype.unique():
         gt_df = sample_df[sample_df.genotype == gt]
         t0 = gt_df.timepoint.min()
-        t0_smp = "{}-T{}".format(gt, t0)
+        t0_smp = "{}-{}".format(gt, t0)
         for tp in gt_df.timepoint.unique():
             for smpl in gt_df[gt_df.timepoint == tp].sample_name.unique():
                 fh.write("\t".join(
-                    [smpl, "{}-T{}".format(gt, tp), t0_smp]) + "\n")
+                    [smpl, "{}-{}".format(gt, tp), t0_smp]) + "\n")
 # write sgrna map
 read_counts = pd.read_csv(snakemake.input[0], sep='\t')
 read_counts[['Target', 'Gene']].to_csv(snakemake.output[1],
