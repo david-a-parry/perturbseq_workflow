50db69a772a68d263c8d4084da924409f410ed7c
David A. Parry <david.parry@ed.ac.uk>
2021-12-17 00:06:38 +0000

    use snakemake defined output files

--- workflow/scripts/combine_read_counts.py
+++ workflow/scripts/combine_read_counts.py
@@ -9,7 +9,7 @@ for csv_input in snakemake.input:
     df = df.append(tmp_df)
 df['sample_name'] = df.filename.apply(
     lambda x: "-".join(os.path.basename(x).split('-')[:-1]))
-df.to_csv("results/read_counts_per_alignment.csv.gz", index=False)
+df.to_csv(snakemake.output[0], index=False)
 count_df = df.groupby(
     ['sample_name', 'Target', 'Gene'])['Coverage'].sum().to_frame()
 count_df.reset_index(inplace=True)
@@ -17,7 +17,7 @@ samp2mapped = count_df.groupby(['sample_name'])['Coverage'].sum().to_dict()
 count_df['fraction_reads'] = count_df.apply(
     lambda x: x.Coverage/samp2mapped[x.sample_name],
     axis=1)
-count_df.to_csv("results/read_counts_per_sample.csv.gz", index=False)
+count_df.to_csv(snakemake.output[1], index=False)
 pivot = df.pivot_table(index=["Target", 'Gene'],
                        columns='sample_name')['Coverage']
 pivot.to_csv(snakemake.output[2], sep='\t')
@@ -26,5 +26,5 @@ sample_df = pd.read_csv(snakemake.config['samples'], sep='\t')
 # write out read counts for each genotype
 for gt in sample_df.genotype.unique():
     sample_ids = list(sample_df[sample_df.genotype == gt].sample_name.values)
-    pivot[sample_ids].to_csv("results/{}_read_counts.tsv".format(gt),
+    pivot[sample_ids].to_csv("results/read_counts/{}_read_counts.tsv".format(gt),
                              sep='\t')
